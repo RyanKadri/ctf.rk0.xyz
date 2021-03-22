1ce6d79eb4e1f3863254c852330f2d983ec4e064
Ryan Kadri <johndoe@example.com>
2021-03-21 23:21:51 -0400

    First

---
+++ src/README.md
@@ -0,0 +1,3 @@
+Can you find the flag? It should be of the format FLAG$#######.
+
+Important: The number after the dollar sign has a particular format. When the digits are multiplied together, the product is 6048
---
+++ src/analytics.js
@@ -0,0 +1,35 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','ftp://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-XXXXX-Y', 'auto');
+ga('send', 'pageview');
+ga('send', 'flagz', env)
+
+var env = {
+    f1: "FLAG$12",
+    f0: "FLAG$9165560",
+    f1: "FLAG$6988977",
+    f2: "FLAG$1027312",
+    f4: "FLAG$4656446",
+    f5: "FLAG$1821374",
+    f6: "FLAG$A1371924",
+    f7: "FLAG$3566864",
+    f8: "FLAG$2287077",
+    f9: "FLAG$8545538",
+    f10: "FLAG$65568Z01",
+    f11: "FLAG$8174873",
+    f12: "FLAG$9438652",
+    f13: "FLAG$1882634",
+    f14: "FLAG$7628877",
+    f15: "FLAG$9694206",
+    f16: "FLAG$7212046",
+    f17: "FLAG$6939799",
+    f18: "FLAG$4336745",
+    f19: "FLAG$6687745",
+}
+
+const script = {
+
+}
